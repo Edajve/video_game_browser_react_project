32889b0f81ae0a80c0699ee1fcb70bfc8f64eaaf
Dajve Echols <dajvelechols@gmail.com>
2023-06-07 01:32:15 -0500

    fetch the games

---
+++ game-hub/src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios , {CanceledError} from "axios";
+
+export default axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "9f281f311d2548dc9ddbb9994bfcac31"
+    }
+})
