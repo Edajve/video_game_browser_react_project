a207e97314525fd521dbe73be5ec0481f44fb517
Dajve Echols <edajve@gmail.com>
2023-06-14 01:40:26 -0500

    clean up game cards

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -15,13 +15,13 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow="hidden" width="275px">
       <Image src={backGroundImage} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack mb={3} justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
